3bf296001c833d6940c9f8b7fe790227d6a04562
Olivier Refalo <orefalo@snapcore.com>
2026-03-16 10:24:14 +0400

    switch

--- src/lib/cossui-components/switch.svelte
+++ src/lib/cossui-components/switch.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
-  import { Switch } from "bits-ui";
+  import { Switch as SwitchPrimitive } from "bits-ui";
   import { cn } from "../utils.js";
 
-  type Props = Switch.RootProps;
+  type Props = SwitchPrimitive.RootProps;
 
   let { class: className, ...restProps }: Props = $props();
 </script>
 
-<Switch.Root
+<SwitchPrimitive.Root
   class={cn(
-    "inline-flex h-[calc(var(--thumb-size)+2px)] w-[calc(var(--thumb-size)*2-2px)] shrink-0 items-center rounded-full p-px outline-none transition-[background-color,box-shadow] duration-200 [--thumb-size:--spacing(5)] focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:ring-offset-background data-[state=checked]:bg-primary data-[state=unchecked]:bg-input data-disabled:opacity-64 sm:[--thumb-size:--spacing(4)]",
+    "inline-flex h-[calc(var(--thumb-size)+2px)] w-[calc(var(--thumb-size)*2-2px)] shrink-0 items-center rounded-full p-px outline-none transition-[background-color,box-shadow] duration-200 [--thumb-size:--spacing(5)] focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:ring-offset-background data-checked:bg-primary data-unchecked:bg-input data-disabled:opacity-64 sm:[--thumb-size:--spacing(4)]",
     className,
   )}
   data-slot="switch"
   {...restProps}
 >
-  <Switch.Thumb
-    class="pointer-events-none block aspect-square h-full origin-left rounded-[var(--thumb-size)] bg-background shadow-sm/5 will-change-transform [transition:translate_.15s,border-radius_.15s,scale_.1s_.1s,transform-origin_.15s] data-[state=checked]:origin-[var(--thumb-size)_50%] data-[state=checked]:translate-x-[calc(var(--thumb-size)-4px)]"
+  <SwitchPrimitive.Thumb
+    class={cn(
+      "pointer-events-none block aspect-square h-full origin-left in-[[role=switch]:active,[data-slot=label]:active,[data-slot=field-label]:active]:not-data-disabled:scale-x-110 in-[[role=switch]:active,[data-slot=label]:active,[data-slot=field-label]:active]:rounded-[var(--thumb-size)/calc(var(--thumb-size)*1.1)] rounded-(--thumb-size) bg-background shadow-sm/5 will-change-transform [transition:translate_.15s,border-radius_.15s,scale_.1s_.1s,transform-origin_.15s] data-checked:origin-[var(--thumb-size)_50%] data-checked:translate-x-[calc(var(--thumb-size)-4px)]",
+    )}
     data-slot="switch-thumb"
   />
-</Switch.Root>
+</SwitchPrimitive.Root>
