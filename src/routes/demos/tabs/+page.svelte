ad957b4f8a3c0c86d32cf83034346ba6a94a106e
Olivier Refalo <orefalo@snapcore.com>
2026-03-19 12:01:19 +0400

    moving app components

--- src/routes/demos/tabs/+page.svelte
+++ src/routes/demos/tabs/+page.svelte
@@ -1,31 +1,39 @@
 <script lang="ts">
-    import {
-        Tabs,
-        TabsList,
-        TabsPanel,
-        TabsTab,
-    } from "$lib/components/ui/tabs";
+    import { Tabs } from "bits-ui";
+    import { onMount } from "svelte";
+
+    let activeElement: HTMLElement = null;
 </script>
 
-<Tabs value="tab-1">
-    <TabsList>
-        <TabsTab value="tab-1">Tab 1</TabsTab>
-        <TabsTab value="tab-2">Tab 2</TabsTab>
-        <TabsTab value="tab-3">Tab 3</TabsTab>
-    </TabsList>
-    <TabsPanel value="tab-1">
-        <p class="p-4 text-center text-muted-foreground text-xs">
-            Tab 1 content
-        </p>
-    </TabsPanel>
-    <TabsPanel value="tab-2">
-        <p class="p-4 text-center text-muted-foreground text-xs">
-            Tab 2 content
-        </p>
-    </TabsPanel>
-    <TabsPanel value="tab-3">
-        <p class="p-4 text-center text-muted-foreground text-xs">
-            Tab 3 content
-        </p>
-    </TabsPanel>
-</Tabs>
+<Tabs.Root class="relative w-full" bind:value let:value>
+    <Tabs.List class="relative flex border-b">
+        <Tabs.Trigger
+            value="tab1"
+            class="px-4 py-2"
+            bind:this={value === "tab1" ? activeElement : null}
+        >
+            Tab 1
+        </Tabs.Trigger>
+
+        <Tabs.Trigger
+            value="tab2"
+            class="px-4 py-2"
+            bind:this={value === "tab2" ? activeElement : null}
+        >
+            Tab 2
+        </Tabs.Trigger>
+
+        {#if activeElement}
+            <div
+                class="absolute bottom-0 h-[2px] bg-blue-600 transition-all duration-300"
+                style="
+          width: {activeElement.offsetWidth}px;
+          left: {activeElement.offsetLeft}px;
+        "
+            />
+        {/if}
+    </Tabs.List>
+
+    <Tabs.Content value="tab1">Tab 1 Content</Tabs.Content>
+    <Tabs.Content value="tab2">Tab 2 Content</Tabs.Content>
+</Tabs.Root>
