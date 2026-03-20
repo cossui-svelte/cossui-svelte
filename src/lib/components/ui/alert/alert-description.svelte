1263c323708d42eeebc13b88eaf58971e903559a
Olivier Refalo <orefalo@snapcore.com>
2026-03-20 10:16:10 +0400

    alert

--- src/lib/components/ui/alert/alert-description.svelte
+++ src/lib/components/ui/alert/alert-description.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
-  import type { Snippet } from "svelte";
-  import type { HTMLAttributes } from "svelte/elements";
-  import { cn } from "$lib/utils.js";
+	import type { HTMLAttributes } from "svelte/elements";
+	import { cn, type WithElementRef } from "$lib/utils.js";
 
-  interface Props extends HTMLAttributes<HTMLDivElement> {
-    children?: Snippet;
-  }
-
-  let { class: className, children, ...restProps }: Props = $props();
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...restProps
+	}: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
 </script>
 
 <div
+bind:this={ref}
   class={cn(
     "flex flex-col gap-2.5 text-muted-foreground [svg~&]:col-start-2",
     className,
