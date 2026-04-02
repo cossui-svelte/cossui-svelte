d49c2f04462973147ececc8a3bdbdd8bae322f85
Olivier Refalo <orefalo@snapcore.com>
2026-04-02 09:49:17 +0400

    working 4

--- src/lib/components/ui/dialog/index.ts
+++ src/lib/components/ui/dialog/index.ts
@@ -3,13 +3,14 @@
 import { Dialog as DialogPrimitive } from 'bits-ui';
 import DialogBackdrop from './dialog-backdrop.svelte';
 import DialogClose from './dialog-close.svelte';
-import DialogPopup from './dialog-content.svelte';
 import DialogDescription from './dialog-description.svelte';
 import DialogFooter from './dialog-footer.svelte';
 import DialogHeader from './dialog-header.svelte';
 import DialogPanel from './dialog-panel.svelte';
+import DialogPopup from './dialog-popup.svelte';
 import DialogTitle from './dialog-title.svelte';
 import DialogTrigger from './dialog-trigger.svelte';
+import DialogViewport from './dialog-viewport.svelte';
 
 const Dialog = DialogPrimitive.Root;
 // const DialogTrigger = DialogPrimitive.Trigger;
@@ -25,9 +26,11 @@ export {
   DialogBackdrop as DialogOverlay,
   DialogHeader,
   DialogPopup,
+  DialogPopup as DialogContent,
   DialogFooter,
   DialogTitle,
   DialogDescription,
   DialogPanel,
+  DialogViewport,
   DialogPrimitive
 };
