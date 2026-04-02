276ad107279a545829f42820ec1e6b6d8d36aafb
Olivier Refalo <orefalo@snapcore.com>
2026-04-02 22:09:59 +0400

    ch

--- src/lib/components/ui/checkbox-group/checkbox-group.svelte
+++ src/lib/components/ui/checkbox-group/checkbox-group.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
   import type { Snippet } from "svelte";
-  import type { HTMLAttributes } from "svelte/elements";
   import { cn } from "$lib/utils.js";
+  import { Checkbox } from "bits-ui";
 
-  interface Props extends HTMLAttributes<HTMLDivElement> {
+  interface Props extends Checkbox.GroupProps {
     children?: Snippet;
   }
 
   let { class: className, children, ...restProps }: Props = $props();
 </script>
 
-<div class={cn("flex flex-col items-start gap-3", className)} {...restProps}>
+<Checkbox.Group
+  class={cn("flex flex-col items-start gap-3", className)}
+  {...restProps}
+>
   {@render children?.()}
-</div>
+</Checkbox.Group>
