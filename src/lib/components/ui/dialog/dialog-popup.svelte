b0220553206cc2a28be9b3441209a61287c58ce2
Olivier Refalo <orefalo@snapcore.com>
2026-04-02 15:34:05 +0400

    s2

--- src/lib/components/ui/dialog/dialog-popup.svelte
+++ src/lib/components/ui/dialog/dialog-popup.svelte
@@ -36,16 +36,18 @@
 		<DialogPrimitive.Content
 			bind:ref
 			class={cn(
-				"bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-1/2 left-1/2 z-50 grid max-h-[calc(100%-2rem)] w-full max-w-[calc(100%-2rem)] -translate-x-1/2 -translate-y-1/2 gap-4 overflow-y-auto rounded-xl border p-6 shadow-lg duration-200 sm:max-w-100",
+				"relative row-start-2 flex max-h-full min-h-0 w-full min-w-0 max-w-lg origin-center flex-col rounded-2xl border bg-popover not-dark:bg-clip-padding text-popover-foreground opacity-[calc(1-var(--nested-dialogs))] shadow-lg/5 transition-[scale,opacity,translate] duration-200 ease-in-out will-change-transform before:pointer-events-none before:absolute before:inset-0 before:rounded-[calc(var(--radius-2xl)-1px)] before:shadow-[0_1px_--theme(--color-black/4%)] data-ending-style:opacity-0 data-starting-style:opacity-0 sm:scale-[calc(1-0.1*var(--nested-dialogs))] sm:data-ending-style:scale-98 sm:data-starting-style:scale-98 dark:before:shadow-[0_-1px_--theme(--color-white/6%)]",
 				bottomStickOnMobile &&
-					"max-sm:grid-rows-[1fr_auto] max-sm:p-0 max-sm:pt-12",
+					"max-sm:max-w-none max-sm:origin-bottom max-sm:rounded-none max-sm:border-x-0 max-sm:border-t max-sm:border-b-0 max-sm:data-ending-style:translate-y-4 max-sm:data-starting-style:translate-y-4 max-sm:before:hidden max-sm:before:rounded-none",
+				className,
 			)}
+			data-slot="dialog-popup"
 			{...restProps}
 		>
 			{@render children?.()}
 			<DialogPrimitive.Close
 				aria-label="Close"
-				class="group focus-visible:border-ring focus-visible:ring-ring/50 absolute top-3 right-3 flex size-7 items-center justify-center rounded transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:pointer-events-none"
+				class="absolute end-2 top-2"
 			>
 				<Button size="icon" variant="ghost">
 					<X />
