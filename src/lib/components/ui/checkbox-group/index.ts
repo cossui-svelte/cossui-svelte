331da32f0448d501817f244a869e56daa2270e9d
Olivier Refalo <orefalo@snapcore.com>
2026-04-02 20:57:57 +0400

    ch

---
+++ src/lib/components/ui/checkbox-group/index.ts
@@ -0,0 +1,2 @@
+import CheckboxGroup from '../checkbox-group/checkbox-group.svelte';
+export { CheckboxGroup };
--- src/lib/components/ui/checkbox/index.ts
+++ src/lib/components/ui/checkbox/index.ts
@@ -1,4 +1,4 @@
-import Checkbox from './checkbox.svelte';
-import CheckboxGroup from './checkbox-group.svelte';
 
-export { Checkbox, CheckboxGroup };
+import Checkbox from './checkbox.svelte';
+
+export { Checkbox };
