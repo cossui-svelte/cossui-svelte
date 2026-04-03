bd451bf865049b2d89dd57ebc042627ca9f9762f
Olivier Refalo <orefalo@snapcore.com>
2026-04-03 21:19:50 +0400

    demo page

--- src/routes/demos/field/+page.svelte
+++ src/routes/demos/field/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import ComponentPreviewTabs from "$lib/components/app/ComponentPreviewTabs.svelte";
     import { Button } from "$lib/components/ui/button";
     import {
@@ -15,8 +15,32 @@
         InputGroupInput,
     } from "$lib/components/ui/input-group";
     import { ArrowRightIcon } from "lucide-svelte";
+
+    function toAnchorLink(text: string): string {
+        return (
+            "#" +
+            text
+                .toLowerCase()
+                .trim()
+                .replace(/\s+/g, "-")
+                .replace(/[^a-z0-9-]/g, "")
+        );
+    }
 </script>
 
+{#snippet header(title: string)}
+    <h3
+        id="required-field"
+        class="mt-8 scroll-m-20 font-semibold text-lg *:[code]:text-lg"
+    >
+        <a
+            class="no-underline underline-offset-4 hover:underline"
+            href={toAnchorLink(title)}>{title}</a
+        >
+    </h3>
+{/snippet}
+
+{@render header("Default")}
 <ComponentPreviewTabs>
     <Field>
         <FieldLabel>Name</FieldLabel>
@@ -25,6 +49,27 @@
     </Field>
 </ComponentPreviewTabs>
 
+{@render header("Required Field")}
+<ComponentPreviewTabs>
+    <Field>
+        <FieldLabel>
+            Password <span class="text-destructive-foreground">*</span>
+        </FieldLabel>
+        <Input placeholder="Enter password" required type="password" />
+        <FieldError>Please fill out this field.</FieldError>
+    </Field>
+</ComponentPreviewTabs>
+
+{@render header("Disabled Field")}
+<ComponentPreviewTabs>
+    <Field disabled>
+        <FieldLabel>Email</FieldLabel>
+        <Input disabled placeholder="Enter your email" type="email" />
+        <FieldDescription>This field is currently disabled.</FieldDescription>
+    </Field>
+</ComponentPreviewTabs>
+
+{@render header("With Error")}
 <ComponentPreviewTabs>
     <Field>
         <FieldLabel>Email</FieldLabel>
@@ -33,6 +78,7 @@
     </Field>
 </ComponentPreviewTabs>
 
+{@render header("With Validity")}
 <ComponentPreviewTabs>
     <Field>
         <FieldLabel>Email</FieldLabel>
@@ -58,6 +104,7 @@
     </Field>
 </ComponentPreviewTabs>
 
+{@render header("Input Group")}
 <ComponentPreviewTabs>
     <Field>
         <FieldLabel>Subscribe</FieldLabel>
