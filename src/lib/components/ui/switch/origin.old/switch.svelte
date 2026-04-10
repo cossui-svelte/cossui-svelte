6b90229bb6112aa2fc6c5ad5bdfc5113f9b56333
Olivier Refalo <orefalo@snapcore.com>
2026-04-10 11:32:04 +0400

    fix checkbox

--- src/lib/components/ui/switch/origin.old/switch.svelte
+++ src/lib/components/ui/switch/origin.old/switch.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import { cn } from '$lib/utils.js';
+	import { cn } from "$lib/utils.js";
 
-	import { Switch as SwitchPrimitive } from 'bits-ui';
+	import { Switch as SwitchPrimitive } from "bits-ui";
 
 	let {
 		checked = $bindable(false),
@@ -15,14 +15,14 @@
 	bind:ref
 	bind:checked
 	class={cn(
-		'peer data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:ring-ring/50 inline-flex h-6 w-10 shrink-0 items-center rounded-full border-2 border-transparent transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50',
-		className
+		"peer data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:ring-ring/50 inline-flex h-6 w-10 shrink-0 items-center rounded-full border-2 border-transparent transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+		className,
 	)}
 	{...restProps}
 >
 	<SwitchPrimitive.Thumb
 		class={cn(
-			'bg-background pointer-events-none block size-5 rounded-full shadow-xs ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0 data-[state=checked]:rtl:-translate-x-4'
+			"bg-background pointer-events-none block size-5 rounded-full shadow-xs ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0 data-[state=checked]:rtl:-translate-x-4",
 		)}
 	/>
 </SwitchPrimitive.Root>
