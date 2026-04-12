2949ec49373998ee49efc24aed2afd0b7f282c29
Olivier Refalo <orefalo@snapcore.com>
2026-04-12 20:23:10 +0400

    input

--- TODO.md
+++ TODO.md
@@ -1,1 +1,2 @@
-* Add avatar badges
+* GROUP bug
+* 
---
+++ src/routes/demos/input/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { Input } from "$lib/components/ui/input";
+    import ComponentPreviewTabs from "$lib/components/app/ComponentPreviewTabs.svelte";
+</script>
+
+<ComponentPreviewTabs>
+    <Input
+        aria-label="Enter text"
+        placeholder="Enter text"
+        size="sm"
+        type="text"
+    />
+</ComponentPreviewTabs>
