9b9e70550136bdd62d8b8320b93e6d4ad10ef194
Olivier Refalo <orefalo@snapcore.com>
2026-04-12 17:46:23 +0400

    checkbox group

--- src/routes/demos/checkbox-group/+page.svelte
+++ src/routes/demos/checkbox-group/+page.svelte
@@ -1,8 +1,39 @@
-<script>
-    import ComponentPreviewTabs from "$lib/components/app/ComponentPreviewTabs.svelte";
-    import { Checkbox } from "$lib/components/ui/checkbox";
+<script lang="ts">
     import { CheckboxGroup } from "$lib/components/ui/checkbox-group";
+
+    import CheckboxTree, {
+        type TreeNode,
+    } from "$lib/components/ui/checkbox-group/checkbox-tree.svelte";
+    import { Checkbox } from "$lib/components/ui/checkbox";
     import { Label } from "$lib/components/ui/label";
+    import ComponentPreviewTabs from "$lib/components/app/ComponentPreviewTabs.svelte";
+
+    const initialTree: TreeNode = {
+        children: [
+            {
+                defaultChecked: true,
+                id: "mountains",
+                indeterminate: true,
+                label: "Mountains",
+            },
+            {
+                children: [
+                    { id: "niagara", label: "Niagara Falls" },
+                    {
+                        defaultChecked: true,
+                        id: "angel-falls",
+                        label: "Angel Falls",
+                    },
+                ],
+                id: "waterfalls",
+                indeterminate: true,
+                label: "Waterfalls",
+            },
+            { id: "grand-canyon", label: "Grand Canyon" },
+        ],
+        id: "natural-wonders",
+        label: "Natural Wonders",
+    };
 </script>
 
 <ComponentPreviewTabs>
@@ -12,7 +43,7 @@
             Next.js
         </Label>
         <Label>
-            <Checkbox value="vite" />
+            <Checkbox disabled value="vite" />
             Vite
         </Label>
         <Label>
@@ -20,4 +51,37 @@
             Astro
         </Label>
     </CheckboxGroup>
+</ComponentPreviewTabs>
+
+<ComponentPreviewTabs>
+    <div class="space-y-3">
+        <CheckboxTree tree={initialTree}>
+            {#snippet renderNode({
+                checked,
+                children,
+                id,
+                indeterminate,
+                label,
+                onCheckedChange,
+            })}
+                <div class="ms-6 flex items-center gap-2">
+                    <Checkbox
+                        {id}
+                        {checked}
+                        {onCheckedChange}
+                        {indeterminate}
+                    />
+                    <Label for={id}>{label}</Label>
+                </div>
+
+                {#if children}
+                    {#each children as child (child.id)}
+                        <div class="ms-6 space-y-3">
+                            {@render renderNode(child)}
+                        </div>
+                    {/each}
+                {/if}
+            {/snippet}
+        </CheckboxTree>
+    </div>
 </ComponentPreviewTabs>
