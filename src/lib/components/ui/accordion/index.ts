028a997308e7e177c97499efbc9db0224ef265e5
Olivier Refalo <orefalo@snapcore.com>
2026-04-13 17:21:08 +0400

    accordion demo

--- src/lib/components/ui/accordion/index.ts
+++ src/lib/components/ui/accordion/index.ts
@@ -1,12 +1,17 @@
+import { Accordion as AccordionPrimitive } from 'bits-ui';
 import Accordion from './accordion.svelte';
 import AccordionItem from './accordion-item.svelte';
 import AccordionPanel from './accordion-panel.svelte';
 import AccordionTrigger from './accordion-trigger.svelte';
 
+const AccordionHeader = AccordionPrimitive.Header;
+
 export {
   Accordion,
+  AccordionHeader,
   AccordionItem,
   AccordionPanel,
   AccordionPanel as AccordionContent,
+  AccordionPrimitive,
   AccordionTrigger
 };
