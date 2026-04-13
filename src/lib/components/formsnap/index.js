4df0c5ff189872d5c8d6270070ebc572f5399090
Olivier Refalo <orefalo@snapcore.com>
2026-04-13 12:54:22 +0400

    remove formsnap

--- src/lib/components/formsnap/index.js
+++
@@ -1,2 +0,0 @@
-export * from './components/index.js';
-export { getFormControl, getFormField, useFormControl, useFormField } from './formsnap.svelte.js';
--- src/lib/components/formsnap/internal/utils/index.js
+++
@@ -1,3 +0,0 @@
-export * from './attributes.js';
-export * from './errors.js';
-export * from './id.js';
