f1509241e4e8b37a39929785aec64733e0af0336
Olivier Refalo <orefalo@snapcore.com>
2026-04-29 10:59:27 +0400

    remove OLD

--- src/lib/old/originui-orig.old/tree/index.ts
+++
@@ -1,8 +0,0 @@
-import Tree from './tree.svelte';
-import TreeAssistiveTreeDescription from './tree-assistive-tree-description.svelte';
-import TreeDragLine from './tree-drag-line.svelte';
-import TreeItem from './tree-item.svelte';
-import TreeLabel from './tree-label.svelte';
-import { useTree } from './use-tree.svelte';
-
-export { Tree, TreeAssistiveTreeDescription, TreeDragLine, TreeItem, TreeLabel, useTree };
