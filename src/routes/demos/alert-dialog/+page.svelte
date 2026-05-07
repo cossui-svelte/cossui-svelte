6be23dd453e5008511fbd8a1371d5a9ab10bd7b6
Olivier Refalo <orefalo@snapcore.com>
2026-05-07 07:43:59 +0400

    fix

--- src/routes/demos/alert-dialog/+page.svelte
+++ src/routes/demos/alert-dialog/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import ComponentPreviewTabs from "$lib/components/app/component-preview-tabs.svelte";
     import {
         AlertDialog,
         AlertDialogClose,
@@ -12,27 +13,29 @@
     import { Button, buttonVariants } from "$lib/components/ui/button";
 </script>
 
-<AlertDialog>
-    <AlertDialogTrigger
-        class={buttonVariants({ variant: "destructive-outline" })}
-    >
-        Delete Account
-    </AlertDialogTrigger>
-    <AlertDialogPopup>
-        <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-            </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-            <AlertDialogClose>
-                <Button variant="ghost">Cancel</Button>
-            </AlertDialogClose>
-            <AlertDialogClose>
-                <Button variant="destructive">Delete Account</Button>
-            </AlertDialogClose>
-        </AlertDialogFooter>
-    </AlertDialogPopup>
-</AlertDialog>
+<ComponentPreviewTabs>
+    <AlertDialog>
+        <AlertDialogTrigger
+            class={buttonVariants({ variant: "destructive-outline" })}
+        >
+            Delete Account
+        </AlertDialogTrigger>
+        <AlertDialogPopup>
+            <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your account and remove your data from our servers.
+                </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+                <AlertDialogClose>
+                    <Button variant="ghost">Cancel</Button>
+                </AlertDialogClose>
+                <AlertDialogClose>
+                    <Button variant="destructive">Delete Account</Button>
+                </AlertDialogClose>
+            </AlertDialogFooter>
+        </AlertDialogPopup>
+    </AlertDialog>
+</ComponentPreviewTabs>
