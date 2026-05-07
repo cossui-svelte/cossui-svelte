0a721e96635d9b37823b59ebe66cc6e86ca00d14
Olivier Refalo <orefalo@snapcore.com>
2026-05-07 17:30:02 +0400

    with shad styles

--- src/lib/components/ui/alert-dialog/alert-dialog-content.svelte
+++ src/lib/components/ui/alert-dialog/alert-dialog-content.svelte
@@ -2,7 +2,11 @@
 	import { AlertDialog as AlertDialogPrimitive } from "bits-ui";
 	import AlertDialogPortal from "./alert-dialog-portal.svelte";
 	import AlertDialogOverlay from "./alert-dialog-overlay.svelte";
-	import { cn, type WithoutChild, type WithoutChildrenOrChild } from "$lib/utils.js";
+	import {
+		cn,
+		type WithoutChild,
+		type WithoutChildrenOrChild,
+	} from "$lib/utils.js";
 	import type { ComponentProps } from "svelte";
 
 	let {
@@ -13,7 +17,9 @@
 		...restProps
 	}: WithoutChild<AlertDialogPrimitive.ContentProps> & {
 		size?: "default" | "sm";
-		portalProps?: WithoutChildrenOrChild<ComponentProps<typeof AlertDialogPortal>>;
+		portalProps?: WithoutChildrenOrChild<
+			ComponentProps<typeof AlertDialogPortal>
+		>;
 	} = $props();
 </script>
 
@@ -24,8 +30,8 @@
 		data-slot="alert-dialog-content"
 		data-size={size}
 		class={cn(
-			"cn-alert-dialog-content group/alert-dialog-content fixed top-1/2 left-1/2 z-50 grid w-full -translate-x-1/2 -translate-y-1/2 outline-none",
-			className
+			"data-open:animate-in data-closed:animate-out data-closed:fade-out-0 data-open:fade-in-0 data-closed:zoom-out-95 data-open:zoom-in-95 bg-popover text-popover-foreground ring-foreground/10 gap-6 rounded-xl p-6 ring-1 duration-100 data-[size=default]:max-w-xs data-[size=sm]:max-w-xs data-[size=default]:sm:max-w-lg group/alert-dialog-content fixed top-1/2 left-1/2 z-50 grid w-full -translate-x-1/2 -translate-y-1/2 outline-none",
+			className,
 		)}
 		{...restProps}
 	/>
