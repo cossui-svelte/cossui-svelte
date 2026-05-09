00225f38a4fe63bc120a764c45cd1d1fb9ea4e95
Olivier Refalo <orefalo@snapcore.com>
2026-05-09 19:12:42 +0400

    fix fetch

---
+++ src/hooks.server.ts
@@ -0,0 +1,14 @@
+import type { Handle } from '@sveltejs/kit';
+import { RequestClient } from '$data/api/helpers/RequestClient';
+
+const typedServerFetch: Handle = async ({ event, resolve }) => {
+    const { fetch, locals } = event;
+
+    if (!locals.serverClient) {
+        locals.serverClient = new RequestClient(fetch);
+    }
+
+    return await resolve(event);
+};
+
+export const handle: Handle = typedServerFetch
