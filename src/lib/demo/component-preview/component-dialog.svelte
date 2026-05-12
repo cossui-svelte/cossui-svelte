66ceb3550ecf22b254df5f6b5387760963621930
Olivier Refalo <orefalo@snapcore.com>
2026-05-12 12:51:39 +0400

    fix import and enrich webmanifast

--- src/lib/demo/component-preview/component-dialog.svelte
+++ src/lib/demo/component-preview/component-dialog.svelte
@@ -7,7 +7,7 @@
 		DialogOverlay,
 		DialogPortal,
 	} from "$lib/components/ui/dialog";
-	import { Drawer, DrawerContent } from "$lib/components/ui/drawer.old";
+	import { Drawer, DrawerContent } from "$lib/components/ui/drawer";
 
 	import { pushState, replaceState } from "$app/navigation";
 	import { page } from "$app/state";
