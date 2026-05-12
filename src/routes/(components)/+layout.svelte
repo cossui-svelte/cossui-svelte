d2ddba3b59d89922281a512e19a153eaf0712e0f
Olivier Refalo <orefalo@snapcore.com>
2026-05-12 22:02:50 +0400

    fixes

--- src/routes/(components)/+layout.svelte
+++ src/routes/(components)/+layout.svelte
@@ -1,18 +1,9 @@
 <script lang="ts">
 	import Cta from "$lib/demo/cta.svelte";
 	import * as ComponentDialog from "$lib/demo/component-preview";
-	import {
-		AnchoredToastProvider,
-		ToastProvider,
-	} from "$lib/components/ui/toast";
+
 	let { children } = $props();
 </script>
-
-<ToastProvider>
-	<AnchoredToastProvider>
-		<main>{@render children()}</main>
-	</AnchoredToastProvider>
-</ToastProvider>
 
 <ComponentDialog.DialogContextProvider>
 	<main class="grow">
