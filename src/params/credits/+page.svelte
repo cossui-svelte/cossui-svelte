623c1f40ecd800e216be95b90ef2261b34381f90
Olivier Refalo <orefalo@snapcore.com>
2026-05-14 22:12:06 +0400

    cleanup

--- src/params/credits/+page.svelte
+++
@@ -1,5 +0,0 @@
-<p>"The most intuitive framework is only as good as its components"</p>
-
-* Coss.com * ShadCN - * OriginUI - * BitsUI * Claude Code - for being such a
-helpful assisstant, always there to explain things and provide generation
-support.
