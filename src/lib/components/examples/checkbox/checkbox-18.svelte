067ac8793cf53276655def29bd6fbbd800b95218
Olivier Refalo <orefalo@snapcore.com>
2026-05-20 21:45:05 +0400

    ch1

--- src/lib/components/examples/checkbox/checkbox-18.svelte
+++ src/lib/components/examples/checkbox/checkbox-18.svelte
@@ -33,27 +33,27 @@ const items = [
 </script>
 
 <fieldset class="space-y-4">
-	<legend class="text-sm font-medium leading-none text-foreground"
-		>Days of the week</legend
-	>
-	<div class="flex gap-1.5">
-		{#each items as item (item.id)}
-			<label
-				class="relative flex size-9 cursor-pointer flex-col items-center justify-center gap-3 rounded-full border border-input text-center shadow-sm shadow-black/[.04] ring-offset-background transition-colors has-[[data-disabled]]:cursor-not-allowed has-[[data-state=checked]]:border-primary has-[[data-state=checked]]:bg-primary has-[[data-state=checked]]:text-primary-foreground has-[[data-disabled]]:opacity-50 has-[:focus-visible]:ring-2 has-[:focus-visible]:ring-ring/70 has-[:focus-visible]:ring-offset-2"
-				for={item.id}
-			>
-				<Checkbox
-					id={item.id}
-					value={item.value}
-					class="sr-only after:absolute after:inset-0"
-					checked={item.defaultChecked}
-					disabled={item.disabled}
-				/>
-				<span aria-hidden="true" class="text-sm font-medium">
-					{item.label[0]}
-				</span>
-				<span class="sr-only">{item.label}</span>
-			</label>
-		{/each}
-	</div>
+  <legend class="text-sm font-medium leading-none text-foreground"
+    >Days of the week</legend
+  >
+  <div class="flex gap-1.5">
+    {#each items as item (item.id)}
+      <label
+        class="relative flex size-9 cursor-pointer flex-col items-center justify-center gap-3 rounded-full border border-input text-center shadow-sm shadow-black/4 ring-offset-background transition-colors has-data-disabled:cursor-not-allowed has-data-[state=checked]:border-primary has-data-[state=checked]:bg-primary has-data-[state=checked]:text-primary-foreground has-data-disabled:opacity-50 has-focus-visible:ring-2 has-focus-visible:ring-ring/70 has-focus-visible:ring-offset-2"
+        for={item.id}
+      >
+        <Checkbox
+          id={item.id}
+          value={item.value}
+          class="sr-only after:absolute after:inset-0"
+          checked={item.defaultChecked}
+          disabled={item.disabled}
+        />
+        <span aria-hidden="true" class="text-sm font-medium">
+          {item.label[0]}
+        </span>
+        <span class="sr-only">{item.label}</span>
+      </label>
+    {/each}
+  </div>
 </fieldset>
