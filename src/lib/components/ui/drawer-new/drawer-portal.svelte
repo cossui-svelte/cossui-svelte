e42a28ffc71cbc1301d8e536624f4d40e86dcf96
Olivier Refalo <orefalo@snapcore.com>
2026-05-28 17:42:47 +0400

    remove old files

--- src/lib/components/ui/drawer-new/drawer-portal.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { Drawer as DrawerPrimitive } from "$lib/vaul";
-
-	let { ...restProps }: DrawerPrimitive.PortalProps = $props();
-</script>
-
-<DrawerPrimitive.Portal {...restProps} />
