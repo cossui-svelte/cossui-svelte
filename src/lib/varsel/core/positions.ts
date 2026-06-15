fb844141261d64ad626d5020b04f6808f632715e
Olivier Refalo <orefalo@snapcore.com>
2026-06-15 14:09:17 +0400

    fix toast animation direction

--- src/lib/varsel/core/positions.ts
+++ src/lib/varsel/core/positions.ts
@@ -8,24 +8,24 @@ export const POSITION_CONFIGS = {
     animateOut: { x: 0, y: 100 }
   },
   'bottom-left': {
-    animateIn: { x: -100, y: 20 },
-    animateOut: { x: -100, y: 100 }
+    animateIn: { x: 0, y: 100 },
+    animateOut: { x: 0, y: 100 }
   },
   'bottom-right': {
-    animateIn: { x: 100, y: 20 },
-    animateOut: { x: 100, y: 100 }
+    animateIn: { x: 0, y: 100 },
+    animateOut: { x: 0, y: 100 }
   },
   'top-center': {
     animateIn: { x: 0, y: -100 },
     animateOut: { x: 0, y: -100 }
   },
   'top-left': {
-    animateIn: { x: -100, y: -20 },
-    animateOut: { x: -100, y: -100 }
+    animateIn: { x: 0, y: -100 },
+    animateOut: { x: 0, y: -100 }
   },
   'top-right': {
-    animateIn: { x: 100, y: -20 },
-    animateOut: { x: 100, y: -100 }
+    animateIn: { x: 0, y: -100 },
+    animateOut: { x: 0, y: -100 }
   }
 } as const;
 
