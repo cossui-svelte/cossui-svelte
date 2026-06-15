0f30e3ad3489cb9218ad7a11f5ac43818817c7f1
Olivier Refalo <orefalo@snapcore.com>
2026-06-15 20:18:12 +0400

    fix vite+pnpm warning on console.log

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,29 @@ import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 // import { visualizer } from 'rollup-plugin-visualizer';
-import { defineConfig } from 'vite';
+import { createLogger, defineConfig } from 'vite';
+
+const logger = createLogger();
+const loggerWarn = logger.warn.bind(logger);
+logger.warn = (msg, options) => {
+  // This is a known pnpm + Vite issue where sourcemap paths resolve to the actual pnpm store location instead of the symlink
+  if (msg.includes('points to a source file outside its package')) return;
+  loggerWarn(msg, options);
+};
 
 export default defineConfig({
   // these @vinejs/vine stuff is there to prevent vine from being bundled in the final build, it was generating errors during the build
   build: {
     rollupOptions: {
-      external: ['@vinejs/vine']
+      external: ['@vinejs/vine'],
+      onwarn(warning, warn) {
+        if (warning.code === 'SOURCEMAP_BROKEN' || warning.message.includes('points to a source file outside its package'))
+          return;
+        warn(warning);
+      }
     }
   },
+  customLogger: logger,
   optimizeDeps: {
     exclude: ['@vinejs/vine']
   },
