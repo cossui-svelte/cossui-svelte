de8550768c78b4b0043117170f5ac447bdc98936
Olivier Refalo <orefalo@snapcore.com>
2026-06-23 20:57:52 +0400

    more particle movements

--- src/lib/components/particles/alert/alert-13.svelte
+++ src/lib/components/particles/alert/alert-13.svelte
@@ -4,14 +4,14 @@
         AlertDescription,
         AlertTitle,
     } from "$lib/components/ui/alert";
-    import ComponentPreviewTabs from "$lib/components/app/docs/component-preview-tabs.svelte";
+
 </script>
 
-<ComponentPreviewTabs>
+
     <Alert>
         <AlertTitle>Heads up!</AlertTitle>
         <AlertDescription>
             <p>Describe what can be done about it here.</p>
         </AlertDescription>
     </Alert>
-</ComponentPreviewTabs>
+
--- src/routes/demos/badge/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import { Badge } from "$lib/components/ui/badge";
-    import ComponentPreviewTabs from "$lib/components/app/docs/component-preview-tabs.svelte";
-</script>
-
-<ComponentPreviewTabs>
-    <Badge>Badge</Badge>
-</ComponentPreviewTabs>
