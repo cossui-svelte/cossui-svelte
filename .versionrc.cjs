8222103e1c6e5d193cf60079dc1754c0a427d217
Olivier Refalo <orefalo@snapcore.com>
2026-07-09 19:06:22 +0400

    chore: pnpm lock and versionrc

---
+++ .versionrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  types: [
+    { type: 'feat', section: '✨ Features' },
+    { type: 'fix', section: '🐛 Bug Fixes' },
+    { type: 'refactor', section: '♻️ Code Refactoring' },
+    { type: 'docs', section: '📝 Documentation' },
+    { type: 'chore', section: '🚚 Chores', hidden: false },
+    { type: 'revert', section: '⏪️ Reverts' }
+  ]
+};
