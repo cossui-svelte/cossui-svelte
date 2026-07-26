29f70f8e8db4f4222e185de0a1f74c4cdd3a6c54
Olivier Refalo <orefalo@snapcore.com>
2026-07-26 09:01:43 +0200

    refactor: we are maturing, remove all .tsx files

--- src/lib/components/particles/slider/p-slider-18.tsx
+++
@@ -1,9 +0,0 @@
-import { Slider } from "@/registry/default/ui/slider";
-
-export default function Particle() {
-  return (
-    <div className="flex h-40 items-center justify-center">
-      <Slider defaultValue={[25, 75]} orientation="vertical" />
-    </div>
-  );
-}
