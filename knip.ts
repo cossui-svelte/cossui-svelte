844af9b92af82d0aa22208c4427a4d39330e459b
Olivier Refalo <orefalo@snapcore.com>
2026-07-28 17:45:14 +0400

    refactor: knip cleaning

--- knip.ts
+++ knip.ts
@@ -3,21 +3,17 @@ const config = {
     svelte: (text: string) => [...text.matchAll(/import[^;]+/gu)].join('\n')
   },
   ignoreIssues: {
-    'src/lib/components/ui/**': ['exports', 'types', 'files'],
-    'src/lib/formsnap/**': ['exports', 'types', 'files'],
+    // 'src/lib/components/ui/**': ['exports', 'types', 'files'],
     'src/lib/varsel/**': ['exports', 'types', 'files'],
-    'src/lib/vaul/**': ['exports', 'types', 'files']
   },
-  entry: [
-    'src/routes/+layout.ts',
-    'src/routes/+layout.server.ts',
-    'src/routes/+page.ts',
-    'src/routes/+page.svelte',
-    'src/routes/+page.server.ts',
-    'src/hooks.server.ts'
-  ],
-  ignoreDependencies: ['@sveltejs/adapter-auto', '@sveltejs/adapter-cloudflare', '@sveltejs/kit'],
-  ignoreUnresolved: ['./$types'],
+  // entry: [
+  //   'src/routes/+layout.ts',
+  //   'src/routes/+page.ts',
+  //   'src/routes/+page.svelte',
+  //   'src/hooks.server.ts'
+  // ],
+  // ignoreDependencies: ['@sveltejs/adapter-auto', '@sveltejs/adapter-cloudflare', '@sveltejs/kit'],
+  // ignoreUnresolved: ['./$types'],
   paths: {
     '$assets/*': ['./src/lib/assets'],
     '$data/*': ['./src/lib/data'],
@@ -27,6 +23,7 @@ const config = {
   svelte: {
     entry: [
       'src/routes/**/+*.{svelte,ts,js}',
+      'src/env.ts',
       'src/params/*.ts',
       'src/app.html',
       'src/lib/**/*.svelte'
