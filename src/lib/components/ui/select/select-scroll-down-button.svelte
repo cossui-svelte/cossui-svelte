6f0e92195973dd42bd95508510df37662b914ca3
Olivier Refalo <orefalo@snapcore.com>
2026-07-28 18:23:18 +0400

    fix: select scroll button 1pixel gaps

--- src/lib/components/ui/select/select-scroll-down-button.svelte
+++ src/lib/components/ui/select/select-scroll-down-button.svelte
@@ -16,7 +16,7 @@
 <SelectPrimitive.ScrollDownButton
 	bind:ref
 	class={cn(
-		"bottom-0 z-50 flex h-6 w-full cursor-default items-center justify-center before:pointer-events-none before:absolute before:inset-x-px before:bottom-px before:h-[200%] before:rounded-b-[calc(var(--radius-lg)-1px)] before:bg-linear-to-t before:from-50% before:from-popover",
+		"absolute bottom-0 z-50 flex h-6 w-full cursor-default items-center justify-center before:pointer-events-none before:absolute before:inset-x-0 before:bottom-0 before:h-[200%] before:rounded-b-[calc(var(--radius-lg)-1px)] before:bg-linear-to-t before:from-50% before:from-popover",
 		className,
 	)}
 	data-slot="select-scroll-down-button"
