e31a9a870f558722b4739d2b1b64af68670afdcb
Olivier Refalo <orefalo@snapcore.com>
2026-08-04 21:51:31 +0400

    fix: input-group has no variants nor size

--- src/lib/components/ui/input-group/input-group-button.svelte
+++ src/lib/components/ui/input-group/input-group-button.svelte
@@ -1,23 +1,4 @@
-<script lang="ts" module>
-  import { tv, type VariantProps } from 'tailwind-variants';
 
-  const inputGroupButtonVariants = tv({
-    base: 'cn-input-group-button flex items-center shadow-none',
-    defaultVariants: {
-      size: 'xs'
-    },
-    variants: {
-      size: {
-        'icon-sm': 'cn-input-group-button-size-icon-sm',
-        'icon-xs': 'cn-input-group-button-size-icon-xs',
-        sm: 'cn-input-group-button-size-sm',
-        xs: 'cn-input-group-button-size-xs'
-      }
-    }
-  });
-
-  export type InputGroupButtonSize = VariantProps<typeof inputGroupButtonVariants>['size'];
-</script>
 
 <script lang="ts">
   import type { Snippet } from 'svelte';
@@ -30,14 +11,10 @@
     class: className,
     children,
     type = 'button',
-    variant = 'ghost',
-    size = 'xs',
     ...restProps
   }: Omit<HTMLButtonAttributes, 'type'> & {
     ref?: HTMLElement | null;
     type?: HTMLButtonAttributes['type'];
-    variant?: ButtonVariant;
-    size?: InputGroupButtonSize;
     children?: Snippet;
     loading?: boolean;
   } = $props();
@@ -46,9 +23,7 @@
 <Button
   bind:ref
   {type}
-  data-size={size}
-  {variant}
-  class={cn(inputGroupButtonVariants({ size }), className)}
+  class={cn(className)}
   {...restProps}
 >
   {@render children?.()}
