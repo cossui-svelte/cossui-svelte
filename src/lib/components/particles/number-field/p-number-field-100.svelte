0dea4e804f0a2fade46a39b83314c7bd07378d70
Olivier Refalo <orefalo@snapcore.com>
2026-08-14 09:12:29 +0200

    fix: small glitch on p-number-field-100

--- src/lib/components/particles/number-field/p-number-field-100.svelte
+++ src/lib/components/particles/number-field/p-number-field-100.svelte
@@ -4,25 +4,26 @@
   import {
     NumberField,
     NumberFieldDecrement,
+    NumberFieldGroup,
     NumberFieldIncrement,
     NumberFieldValue
   } from '$lib/components/ui/number-field';
 </script>
 
 <NumberField class="w-auto" value={235}>
-  <div class="-space-x-px inline-flex rounded-md shadow-xs rtl:space-x-reverse">
+  <NumberFieldGroup>
     <NumberFieldIncrement
       aria-label="Upvote"
-      class="size-9 rounded-none border border-input px-0 shadow-none first:rounded-s-md last:rounded-e-md focus-visible:z-10 sm:size-8"
+      class="size-9 rounded-none px-0 first:rounded-s-[calc(var(--radius-lg)-1px)] last:rounded-e-[calc(var(--radius-lg)-1px)] sm:size-8"
     >
       <ChevronUpIcon aria-hidden="true" size={16} />
     </NumberFieldIncrement>
-    <NumberFieldValue class="border border-input px-3" />
+    <NumberFieldValue class="px-3" />
     <NumberFieldDecrement
       aria-label="Downvote"
-      class="size-9 rounded-none border border-input px-0 shadow-none first:rounded-s-md last:rounded-e-md focus-visible:z-10 sm:size-8"
+      class="size-9 rounded-none px-0 first:rounded-s-[calc(var(--radius-lg)-1px)] last:rounded-e-[calc(var(--radius-lg)-1px)] sm:size-8"
     >
       <ChevronDownIcon aria-hidden="true" size={16} />
     </NumberFieldDecrement>
-  </div>
+  </NumberFieldGroup>
 </NumberField>
