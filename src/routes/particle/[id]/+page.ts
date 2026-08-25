00b63c4763dae876fa310ba472d2a92a8104d4e5
Olivier Refalo <orefalo@snapcore.com>
2026-08-25 16:04:03 +0400

    fix: ssr rendering - exposed entries

--- src/routes/particle/[id]/+page.ts
+++ src/routes/particle/[id]/+page.ts
@@ -1,6 +1,10 @@
 import { error } from '@sveltejs/kit';
 import { allParticles } from '$lib/registry/registry-particles';
-import type { PageLoad } from './$types';
+import type { EntryGenerator, PageLoad } from './$types';
+
+export const entries: EntryGenerator = () => {
+  return Object.keys(allParticles).map((id) => ({ id }));
+};
 
 export const load: PageLoad = ({ params }) => {
   const particle = allParticles[params.id];
